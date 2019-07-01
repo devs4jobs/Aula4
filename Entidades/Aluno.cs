2ce0b14db962bed4aa5545d4757e81baaaba3fec
guibolt <guilhermedosreiss@gmail.com>
2019-07-01 17:05:39 -0300

    terceiro commit

--- Entidades/Aluno.cs
+++ Entidades/Aluno.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-
+using CadastroEscolar.Entidades.Enums;
 namespace CadastroEscolar.Entidades
 {
     class Aluno:Pessoa
     {
+        public List<Classes> lstCodAlunos{ get; set; } = new List<Classes>();
         public int Ra { get;  set; }
         public object Alunos { get;  set; }
 
@@ -27,7 +28,6 @@ namespace CadastroEscolar.Entidades
             Console.WriteLine("DIGITE O NOME DO ALUNO");
             aluno.Nome = Console.ReadLine();
 
-
             Console.WriteLine("DIGITE A IDADE DO ALUNO:");
             aluno.Idade = int.Parse(Console.ReadLine());
 
@@ -40,6 +40,11 @@ namespace CadastroEscolar.Entidades
 
             Console.WriteLine("CRIE UM RA PARA ESTE ALUNO: ");
             aluno.Ra = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("QUAL É A TURMA DO Aluno? A,B,C,D ou E");
+            Classes C = Enum.Parse<Classes>(Console.ReadLine().ToUpper());
+            lstCodAlunos.Add(C);
+
 
             return aluno;
 
--- Program.cs
+++ Program.cs
@@ -40,8 +40,6 @@ namespace CadastroEscolar
                     Console.WriteLine(professor.Nome);
                 }
 
-
-
             }
 
         }
