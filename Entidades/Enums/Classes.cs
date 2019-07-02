2024ba3c936d87bc3fb9c6a1693cb95d40220881
guibolt <guilhermedosreiss@gmail.com>
2019-07-02 09:19:03 -0300

    comecar

---
+++ Entidades/Enums/Classes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroEscolar.Entidades.Enums
+{
+    enum Classes:int
+    {
+        A=0,
+        B=1,
+        C=2,
+        D=3,
+        E=4,
+    }
+}
