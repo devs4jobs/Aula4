2c7b696d4ce3c738dd15412f4092b1f7a022b592
guibolt <guilhermedosreiss@gmail.com>
2019-07-05 20:16:31 -0300

    m

---
+++ Entidades/Design/Designs.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CadastroEscolar.Entidades.Design
+{
+    class Designs
+    {
+        public void MudarCores()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        public void MudarCores1()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        public void MudarCores2()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+        public void MudarBack()
+        {
+            Console.Title = ("CADASTRO ESCOLAR PARA PROFESSORES E ALUNO");
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Clear();
+        }
+        public void WriteLineCenter(string a)
+        {
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (a.Length / 2)) + "}", a));
+        }
+    }
+}
